d8925ad968f9d5a895fd01b35cc2d146748de980
Corentin Girard <girard_corentin@outlook.fr>
2022-05-29 14:54:44 +0200

    Added missing repository and refactored existing one

--- src/Entity/User/BaseUser.php
+++ src/Entity/User/BaseUser.php
@@ -4,7 +4,7 @@ namespace App\Entity\User;
 
 use App\Entity\Offer;
 use App\Entity\Order;
-use App\Repository\UserRepository;
+use App\Repository\BaseUserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Entity(repositoryClass: BaseUserRepository::class)]
 class BaseUser implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
--- src/Repository/BaseUserRepository.php
+++ src/Repository/BaseUserRepository.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  * @method BaseUser[]    findAll()
  * @method BaseUser[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class BaseUserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
